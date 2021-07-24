b64b9615180f125704f9c0ec78fb4af492780488
tonypham90 <tuananh.gt@hotmail.com>
2021-07-24 15:58:37 +0700

    bai tap nhom

---
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/baitap2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace baitap2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
