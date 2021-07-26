b004d61d02778f9a7a1349fc42d3ecde312b48a1
tonypham90 <tuananh.gt@hotmail.com>
2021-07-26 21:46:10 +0700

    ok

---
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/Baitap1/Baitap1/Baitap1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Baitap1
+{
+    class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Bai tap 1:\nBài 1: Tính S(n) = 1 + 2 + 3 + … + n");
+            int s = 0, i=0;
+            Console.WriteLine("nhap so vong lap:");
+            var n= int.Parse(Console.ReadLine()!);
+            while (i < n)
+            {
+                i++;
+                s += i;
+            }
+            Console.WriteLine($"ket qua cua bai toan la: {s}");
+            
+        }
+    }
+}
