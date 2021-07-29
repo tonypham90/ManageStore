513d75d8e71cfbaa87272075309365b62267e296
tonypham90 <tuananh.gt@hotmail.com>
2021-07-29 22:09:31 +0700

    ok

---
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/Baitap1/Baitap1/Baitap63/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Baitap63
+{
+    class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bài 63: Cho 2 số nguyên dương a và b. Hãy tìm bội chung nhỏ nhất của 2 số này");
+            Console.WriteLine("Nhap so a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so b");
+            int b = int.Parse(Console.ReadLine());
+            int i = Math.Max(a,b);
+            int minboiso = 0;
+            while (minboiso==0)
+            {
+                i++;
+                // Console.WriteLine(i);
+                if (i % a == 0 && i %b == 0)
+                {
+                    minboiso = i;
+                    break;
+                }
+                
+            }
+            Console.WriteLine($"uoc so lon nhat cua {a} va {b} la {minboiso}");
+
+        }
+    }
+}
