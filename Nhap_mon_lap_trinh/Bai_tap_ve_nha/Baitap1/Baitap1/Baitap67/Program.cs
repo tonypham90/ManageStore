0a640f86f3c316af1843f8a37b1a384798a33bed
tonypham90 <tuananh.gt@hotmail.com>
2021-07-31 06:11:50 +0700

    ok

---
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/Baitap1/Baitap1/Baitap67/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+
+namespace Baitap67
+{
+    class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bài 67: Tính S(x, n) = x – x^2 + x^3 + … + (-1)^n+1 * x^n");
+            double s = 0, n,x;
+            double i = 0;
+            Console.WriteLine("Nhap so lan tinh n");
+            n = double.Parse(Console.ReadLine()!);
+            Console.WriteLine("Nhap so lan tinh n");
+            x = double.Parse(Console.ReadLine()!);
+            while (i < n)
+            {
+                
+                
+                i++;
+                // ReSharper disable once PossibleLossOfFraction
+                s += (Math.Pow(-1,i+1) * Math.Pow(x,i));
+            }
+            Console.WriteLine($"Kết quả bài toán là: {s:F}");
+        }
+    }
+}
