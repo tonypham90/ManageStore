512f70df16139df2f4f60ae17eac46907202769d
tonypham90 <tuananh.gt@hotmail.com>
2021-07-31 07:02:00 +0700

    push 101

---
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/Baitap1/Baitap1/Baitap 101/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Baitap_101
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int thang, nam, ngay;
+            Console.WriteLine("xac dinh so ngay trong thang");
+            Console.WriteLine("Nhap THANG");
+            thang = int.Parse(Console.ReadLine() ?? string.Empty);
+            Console.WriteLine("Nhap nam");
+            nam = int.Parse(Console.ReadLine() ?? string.Empty);
+            //thang 31 ngay: 1,3,5,7,8,10,12
+            //thang 30 ngay: 4,6,9,11
+            //thang 28 or 29: 2
+
+            if (thang == 2)
+                if ((nam % 4 == 0 && nam % 100 != 0) || (nam % 100 == 0 && nam % 400 == 0))
+                {
+                    ngay = 29;
+                }
+                else
+                {
+                    ngay = 28;
+                }
+            else if ((thang == 1 )|| thang == 3 || thang == 5|| thang == 7 || thang ==8 || thang == 10||thang == 12)
+            {
+                ngay = 31;
+            }
+            else
+            {
+                ngay = 30;
+            }
+            Console.WriteLine($"Ngay cua thang {thang} nam {nam} la: {ngay} ngay");
+        }
+    }
+}
---
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/Baitap1/Baitap1/Baitap72/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Baitap72
+{
+    class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tính S(x, n) = – x + x^2/2! – x^3/3! + … + (-1)^n * x^n/n!");
+            double s = 0, n, x, giaithua = 1;
+            double i = 0, a;
+            Console.WriteLine("Nhap so lan tinh n");
+            n = double.Parse(Console.ReadLine()!);
+            Console.WriteLine("Nhap so lan tinh x");
+            x = double.Parse(Console.ReadLine()!);
+            while (i < n)
+            {
+                //tính n!
+                a = 0;
+                while (a < i)
+                {
+                    a++;
+                    giaithua *= a;
+                }
+
+                i++;
+                // ReSharper disable once PossibleLossOfFraction
+                s += (Math.Pow(-1, i) * Math.Pow(x, i)) / (giaithua);
+            }
+
+            Console.WriteLine($"Kết quả bài toán là: {s:F}");
+        }
+    }
+}
