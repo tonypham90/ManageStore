68e6c12f85c25b63cfd146ab655bb5354d6d8847
tonypham90 <tuananh.gt@hotmail.com>
2021-07-31 08:17:31 +0700

    ok

---
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/Baitap1/Baitap1/Baitap82/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Baitap82
+{
+    class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bài 82: Viết chương trình tìm số lớn nhất trong 3 số thực a, b, c");
+            double a, b, c,max;
+            Console.WriteLine("Nhap gia tri a");
+            a=double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap gia tri b");
+            b=double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap gia tri c");
+            c=double.Parse(Console.ReadLine());
+            if (a>=b && a>= c)
+            {
+                max = a;
+            }
+            else if (b>= c)
+            {
+                max = b;
+            }
+            else
+            {
+                max = c;
+            }
+            Console.WriteLine($"Giá trị lớn nhất của chuỗi số là: {max}");
+        }
+    }
+}
