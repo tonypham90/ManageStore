e7089647f68547aa3e77ea79833424ef8dd68a96
tonypham90 <tuananh.gt@hotmail.com>
2021-08-14 16:03:42 +0700

    Baitapvenha.cs update class

--- Nhap_mon_lap_trinh/thuc_hanh_tren_lop/thuchanh/bai20/Program.cs
+++ Nhap_mon_lap_trinh/thuc_hanh_tren_lop/thuchanh/bai20/Program.cs
@@ -5,6 +5,8 @@ namespace bai20
     class Program
     {
         // ReSharper disable once IdentifierTypo
+        // ReSharper disable once ArrangeTypeMemberModifiers
+        // ReSharper disable once UnusedMember.Local
         static int Kiemtrauoc(int n)
         {
             for (int i = 1; i < n; i++)
@@ -32,21 +34,33 @@ namespace bai20
             }
             return check;
         }
+
+        static void Hoanvi (ref int a,ref int b)
+        {
+            var t = b;
+            b = a;
+            a = t;
+        }
+        // ReSharper disable once UnusedParameter.Local
         static void Main(string[] args)
         {
             Console.Write("tong cac so nguyen to cua n = ");
-            int n = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine()!);
+            int y = int.Parse(Console.ReadLine()!);
             var total = 0;
-            for (int i = 1; i < n; i++)
-            {
-                bool a = Songuyento(i);
-                if (a)
-                { 
-                    Console.WriteLine($"là số {i} nguyên tố");
-                    total += i;
-                }
-            }
-            Console.WriteLine($"Tong so nguyen to la {total}");
+            // for (int i = 1; i < n; i++)
+            // {
+            //     bool a = Songuyento(i);
+            //     if (a)
+            //     { 
+            //         Console.WriteLine($"là số {i} nguyên tố");
+            //         total += i;
+            //     }
+            
+            // }
+            Console.WriteLine($"Tong so nguyen to la {x} {y}");
+            Hoanvi(ref x, ref y);
+            Console.WriteLine($"Tong so nguyen to la {x} {y}");
         }
     }
 }
---
+++ Nhap_mon_lap_trinh/thuc_hanh_tren_lop/thuchanh/thuchanh/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace thuchanh
+{
+    class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tinh giai thua n!");
+            Console.Write("nhap gia tri n= ");
+            var n =int.Parse(Console.ReadLine()!);
+            n = Math.Abs(n);
+            Baitapvenha.KiemTraThanhPhanLe(n);
+            // Console.WriteLine($"Ket qua tinh la {n}");
+            // Console.Write("tong cac so nguyen to cua n = ");
+            // int x = int.Parse(Console.ReadLine()!);
+            // int y = int.Parse(Console.ReadLine()!);
+            // var total = 0;
+            // // for (int i = 1; i < n; i++)
+            // // {
+            // //     bool a = Songuyento(i);
+            // //     if (a)
+            // //     { 
+            // //         Console.WriteLine($"là số {i} nguyên tố");
+            // //         total += i;
+            // //     }
+            //
+            // // }
+            // Console.WriteLine($"Tong so nguyen to la {x} {y}");
+            // xulysonguyen.Hoanvi(ref x, ref y);
+            // Console.WriteLine($"Tong so nguyen to la {x} {y}");
+        }
+    }
+}
