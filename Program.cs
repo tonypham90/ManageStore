ba0812d7596f23e276c087cd3020774bfc457a74
tonypham90 <tuananh.gt@hotmail.com>
2021-09-04 10:28:50 +0700

    ok

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace khoahoctunhien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
