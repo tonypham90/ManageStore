dda7426942ff82a4dee38f22f90537b8b8bf7b08
tonypham90 <tuananh.gt@hotmail.com>
2021-09-18 15:02:07 +0700

    change

---
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/Bai_tap_mang_v2/Bai_tap_mang_v2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bai_tap_mang_v2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Giai bai tap mang 1 chieu");
+            Console.Write("Bai tap so: ");
+            var input = Console.ReadLine()!;
+            switch (input)
+            {
+                case "122":
+                    practice_source.bai122();
+                    break;
+                case "123":
+                    practice_source.bai123();
+                    break;
+            }
+        }
+    }
+}
---
+++ Nhap_mon_lap_trinh/bai_tap_mang/bai_tap_mang/bai_tap_mang/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Net.Http.Headers;
+
+namespace bai_tap_mang
+{
+    class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bai tap mang 1 chieu");
+            Console.Write("tao mang tu dong voi gia tri nho nhat= ");
+            int minVal = int.Parse(Console.ReadLine()!);
+            Console.Write("tao mang tu dong voi gia tri lon nhat= ");
+            int maxVal = int.Parse(Console.ReadLine()!);
+            int [] man = array_analysis.tao_array_int(minVal,maxVal);
+            BaiTap.bai140(man);
+            BaiTap.bai173(man);
+        }
+        
+    }
+}
