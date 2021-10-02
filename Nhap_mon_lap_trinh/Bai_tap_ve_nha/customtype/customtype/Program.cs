3a072e8d02c4652b03c09124c933c2574b2080b1
tonypham90 <tuananh.gt@hotmail.com>
2021-10-02 16:06:02 +0700

    ok

---
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/customtype/customtype/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace customtype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*PHANSO a,b;
+            a = xulyphanso.inputPhanSo("Nhap Phan so a: ");
+            b = xulyphanso.inputPhanSo("Nhap phan so b: ");
+            //xulyphanso.xuatPhanSo(a);
+            PHANSO kq;
+            kq = xulyphanso.Cong2PhanSo(a, b);
+            xulyphanso.xuatPhanSo(kq);*/
+
+            TOADO a, b, c;
+            a = dothi.nhapToaDo("Nhap Toa do diem A: ");
+            b = dothi.nhapToaDo("Nhap Toa do diem B: ");
+            c = dothi.nhapToaDo("Nhap Toa do diem C: ");
+            float chuvi;
+            chuvi = dothi.SPACING(a, b) + dothi.SPACING(b, c) + dothi.SPACING(a, c);
+            Console.WriteLine($"chuvi tam giac A B C la: {chuvi:F}");
+
+        }
+    }
+}
