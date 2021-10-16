c052d880af25b17b50b5d6a0f1f19542625c79b8
tonypham90 <tuananh.gt@hotmail.com>
2021-10-16 23:32:16 +0700

    ok

---
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/ManageStore/ManageStore/item.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ManageStore
+{
+    struct itemlist
+    {
+        public int nametype()
+    }
+    public class item
+    {
+        public 
+    }
+}
---
+++ Nhap_mon_lap_trinh/Bai_tap_ve_nha/test/test/MonoBehaviour1.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace test
+{
+    public class MonoBehaviour1 : MonoBehaviour
+    {
+    }
+}
